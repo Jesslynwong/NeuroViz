cddd08540dda2c80299e4ce2fd8dfde168ac4ea5
Jesslynwong <jesslynwjx@gmail.com>
2024-10-31 16:54:31 +0800

    feat: del dummy file

--- Idea_core/core_client.py
+++
@@ -1,5 +0,0 @@
-from Pre_process_for_Model import *
-
-
-
-get_response_Idea()
--- util/models.py
+++
@@ -1,8 +0,0 @@
-import json
-from typing import List, Optional
-from pydantic import BaseModel
-
-class Response_Excel_Range(BaseModel):
-    linking_elem: str
-    element_selector: str
-    element_interactive: bool
