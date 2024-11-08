f37ed40d46efb07e9c8cbd2daf4cd54c735537dd
0753Ljuc <limequery@163.com>
2024-11-08 14:34:45 +0800

    fix: catch error for fetch

--- Frontend/src/config/configuration.ts
+++ Frontend/src/config/configuration.ts
@@ -4,7 +4,11 @@ export const productName = "NeuroViz";
 
 export const departmentName = "C2E";
 
-export const host = "http://47.237.98.232:7777";
+// export const host = "http://47.237.98.232:7777";
+export const host =
+  process.env.NODE_ENV === "production"
+    ? "https://nueralviz.online/"
+    : "http://47.237.98.232:7777";
 
 export const uploadConfig = {
   maxHistoryCount: 5,
