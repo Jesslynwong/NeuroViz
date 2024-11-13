fecff4ca2eb332e0b185f129afa5794bb49e490e
0753Ljuc <limequery@163.com>
2024-11-13 14:00:50 +0800

    build: update host url

--- Frontend/src/config/configuration.ts
+++ Frontend/src/config/configuration.ts
@@ -4,7 +4,8 @@ export const productName = "NeuroViz";
 
 export const departmentName = "C2E";
 
-export const host = "http://47.237.98.232:7777";
+export const host = "https://backendtest.neuroviz.online";
+// export const host = "http://47.237.98.232:7777";
 export const uploadConfig = {
   maxHistoryCount: 5,
 } as const;
